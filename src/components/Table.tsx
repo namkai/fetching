f5dd4d7de1040768d0447a4251cd73a01f45be94
namkaif <namkai.fairfield@hired.com>
2020-07-21 17:13:16 -0700

    First commit

---
+++ src/components/Table.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`
+
+interface TableProps {
+
+}
+
+const enum
+
+export default function Table(): React.FC<TableProps> {
+  const []
+  return (
+    <Container>
+      Table
+    </Container>
+  )
+}
